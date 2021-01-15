1b4840f3ab399ce63bf3bd1e5b644bd1697ae6e1
Paul Okello <paulokello273@gmail.com>
2021-01-15 09:35:11 +0300

    postMessage mutation 🇯🇯🇯🇯🇯

--- server/server.js
+++ server/server.js
@@ -10,11 +10,26 @@ type Message {
 type Query{
     messages: [Message!]
 }
+type Mutation{
+    postMessage(user: String!, content:String!):ID!
+}
 `;
 
 const resolvers = {
   Query: {
     messages: () => messages,
+  },
+  Mutation: {
+    postMessage: (parent, { user, content }) => {
+      const id = messages.length;
+
+      messages.push({
+        id,
+        user,
+        content,
+      });
+      return id;
+    },
   },
 };
 
