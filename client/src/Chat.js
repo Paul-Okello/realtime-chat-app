f40207e1f0012b257db37afa1ad86dd9be7e0b10
Paul Okello <paulokello273@gmail.com>
2021-01-18 17:49:41 +0300

    🏘🏘Chat component building 🏘🏘

--- client/src/Chat.js
+++ client/src/Chat.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   ApolloClient,
   InMemoryCache,
@@ -6,6 +6,7 @@ import {
   gql,
   useQuery,
 } from "@apollo/client";
+import { Col, Container, Row } from "shards-react";
 
 const client = new ApolloClient({
   uri: "http://localhost:4000/",
@@ -25,18 +26,65 @@ const GET_MESSAGES = gql`
 const Messages = ({ user }) => {
   const { data } = useQuery(GET_MESSAGES);
 
-  if (data) {
+  if (!data) {
     return null;
   }
   console.log(data);
-  return JSON.stringify(data);
+  return (
+    <>
+      {data.messages.map(({ id, user: messageUser, content }) => (
+        <div
+          style={{
+            display: "flex",
+            justifyContent: user === messageUser ? "flex-end" : "flex-start",
+            paddingBottom: "1em",
+          }}
+        >
+          {user != messageUser && (
+            <div
+              style={{
+                height: 50,
+                width: 50,
+                marginRight: "0.5em",
+                border: "2px solid #e5e6ea",
+                borderRadius: 25,
+                textAlign: "center",
+                fontSize: "18pt",
+                paddingTop: 5,
+              }}
+            >
+              {messageUser.slice(0, 2).toUpperCase()}
+            </div>
+          )}
+          <div
+            style={{
+              background: user === messageUser ? "#58bf56" : "#e5e6ea",
+              color: user === messageUser ? "white" : "black",
+              padding: "1em",
+              borderRadius: "1em",
+              maxWidth: "60%",
+            }}
+          >
+            {content}
+          </div>
+        </div>
+      ))}
+    </>
+  );
 };
 
 const Chat = () => {
+  const [state, setState] = useState({
+    user: "Lopoz",
+    content: "",
+  });
   return (
-    <div>
-      <Messages user="lopoz" />
-    </div>
+    <Container>
+      <Messages user={state.user} />
+      <Row>
+        <Col xs={12} style={{ padding: 0 }}></Col>
+      </Row>
+    </Container>
   );
 };
 
